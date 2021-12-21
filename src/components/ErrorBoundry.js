20171ee33685899b4dfd8fdad67874adfbb76215
Brad Wisesa <brad@grip-principle.com>
2021-12-21 23:22:01 +0700

    Update

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -8,9 +8,9 @@ class ErrorBoundry extends Component {
         }
     }
 
-    componentDidCatch(error, info){
-        this.setState({hasError: true})
-    }
+    // componentDidCatch(error, info){
+    //     this.setState({hasError: true})
+    // }
 
     render() {
         if(this.state.hasError){
